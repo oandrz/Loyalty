bb785cd4600bbcad48ee39ac7876f5f1c6aacd50
Andreas <andreas@ovo.id>
2020-12-21 18:26:59 +0700

    Add Otp Frame

---
+++ app/src/main/kotlin/com/dre/loyalty/features/otp/OtpActivity.kt
@@ -0,0 +1,17 @@
+/*
+ *  Created by Andreas Oentoro on 12/20/20 4:56 PM
+ *  Copyright (c) 2020 . All rights reserved.
+ *  Last modified 12/20/20 4:56 PM
+ *  Github Profile: https://github.com/oandrz
+ */
+
+package com.dre.loyalty.features.otp
+
+import com.dre.loyalty.core.platform.BaseActivity
+import com.dre.loyalty.core.platform.BaseFragment
+
+class OtpActivity : BaseActivity() {
+    override fun fragment(): BaseFragment {
+        TODO("Not yet implemented")
+    }
+}
